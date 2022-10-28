904400d7ebb66af3ba0da45bb799dc82bc4da947
lakshman <nyludaya@gmail.com>
2022-10-28 21:35:57 +0800

    initial commit

---
+++ src/main/java/com/singtel/animalqueryapi/AnimalQueryApiApplication.java
@@ -0,0 +1,13 @@
+package com.singtel.animalqueryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalQueryApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnimalQueryApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singtel/animalqueryapi/AnimalQueryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singtel.animalqueryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalQueryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
